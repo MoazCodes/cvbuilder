8a51bb14e0bcc83b6fb857b9cb91fa45f6f2931f
abdelrahman <abdelrahmanosama372@gmail.com>
2024-10-12 10:47:53 +0300

    fix wrong folder name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import logo from "./logo.svg";
-import Home from "./Pages/Home/Home";
+import Home from "./pages/Home/Home";
 import Navbar from "./componenets/Navbar";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -8,11 +8,11 @@ import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import "animate.css/animate.min.css";
 // @ts-ignore
 import WOW from "wowjs";
-import Templates from "./Pages/Templates/Templates";
+import Templates from "./pages/Templates/Templates";
 import { Route, Routes } from "react-router-dom";
 import CvInputs from "./componenets/CvInputs/CvInputs";
-import Register from "./Pages/Register";
-import Applications from "./Pages/Applications/Applications";
+import Register from "./pages/Register";
+import Applications from "./pages/Applications/Applications";
 function App() {
     useEffect(() => {
         new WOW.WOW({
