ad2a5c378d33a0dc7a94caba5713c4a05147b9af
Moaz-Esmail <moazmohamed.ba@gmail.com>
2024-10-14 17:03:32 +0300

    fix: change to camel case in add and retrieve cv

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import signup_view, login_view,add_cv,retriveCv,edit_cv
+from .views import signup_view, login_view,add_cv,retrieveCv,editCv
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('addcv/', add_cv, name='add_cv'),
-    path('cv/<int:id>', retriveCv, name='retriveCv'), 
-    path('editcv/', edit_cv, name='edit_cv'), 
+    path('cv/<int:id>', retrieveCv, name='retrieveCv'), 
+    path('editcv/', editCv, name='editCv'), 
 ]
